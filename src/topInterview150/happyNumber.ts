b1cbb08716aee034b051944608d867157bdfd723
ShotaroSuzu <cosmos.1220.3110@gmail.com>
2024-02-01 01:35:03 +0000

    refactor: change indent from tab to space

--- src/topInterview150/happyNumber.ts
+++ src/topInterview150/happyNumber.ts
@@ -1,33 +1,33 @@
 export function isHappy(n: number): boolean {
-	// １桁にバラす
-	// 案1. 数字を文字にする
-	// 案2. 1以下になるまで10で割り続ける
-	// それぞれ２乗して足す
-	// 判定
-	// 1だったらtrueを返す
-	// それ以外ならSetにあるかチェック
-	// なければSetに積んで戻る
-	// あればfalseを返す
-	const existingNumbers = new Set();
-	let target = n;
-	while (true) {
-		const happyCandidate = target
-			.toString()
-			.split("")
-			.map((s) => {
-				return Number.parseInt(s);
-			})
-			.reduce((sum, e) => {
-				return sum + e ** 2;
-			}, 0);
+  // １桁にバラす
+  // 案1. 数字を文字にする
+  // 案2. 1以下になるまで10で割り続ける
+  // それぞれ２乗して足す
+  // 判定
+  // 1だったらtrueを返す
+  // それ以外ならSetにあるかチェック
+  // なければSetに積んで戻る
+  // あればfalseを返す
+  const existingNumbers = new Set();
+  let target = n;
+  while (true) {
+    const happyCandidate = target
+      .toString()
+      .split("")
+      .map((s) => {
+        return Number.parseInt(s);
+      })
+      .reduce((sum, e) => {
+        return sum + e ** 2;
+      }, 0);
 
-		if (happyCandidate === 1) {
-			return true;
-		}
-        if (existingNumbers.has(happyCandidate)) {
-			return false;
-		}
-		existingNumbers.add(happyCandidate);
-		target = happyCandidate;
-	}
+    if (happyCandidate === 1) {
+      return true;
+    }
+    if (existingNumbers.has(happyCandidate)) {
+      return false;
+    }
+    existingNumbers.add(happyCandidate);
+    target = happyCandidate;
+  }
 }
--- src/topInterview150/linkedListCycle.ts
+++ src/topInterview150/linkedListCycle.ts
@@ -11,22 +11,22 @@
  */
 
 export class ListNode {
-	val: number;
-	next: ListNode | null;
-	constructor(val?: number, next?: ListNode | null) {
-		this.val = val === undefined ? 0 : val;
-		this.next = next === undefined ? null : next;
-	}
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
 }
 export function hasCycle(head: ListNode | null): boolean {
-	const existingVals = new Set();
-	let currentNode = head;
-	while (!!currentNode && currentNode.next !== null) {
-		if (existingVals.has(currentNode)) {
-			return true;
-		}
-		existingVals.add(currentNode);
-		currentNode = currentNode.next;
-	}
-	return false;
+  const existingVals = new Set();
+  let currentNode = head;
+  while (!!currentNode && currentNode.next !== null) {
+    if (existingVals.has(currentNode)) {
+      return true;
+    }
+    existingVals.add(currentNode);
+    currentNode = currentNode.next;
+  }
+  return false;
 }
